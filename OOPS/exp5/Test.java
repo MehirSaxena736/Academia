38520d15ddcf8c0f6cf1c19e7f2f9d61ccf075e6
Mehir Saxena <mehirsaxena7@gmail.com>
2026-06-29 23:08:19 +0530

    Delete OOPS directory

--- OOPS/exp5/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String args[]){
-    String s1= "Advanced";
-    String s2= s1+ "Programming";
-    s1 ="Java"+"Language";
-    s2.concat(s1);
-    System.out.println(s1);
-    System.out.println(s2);}}
